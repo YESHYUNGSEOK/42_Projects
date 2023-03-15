e9f97e8dcd295d6dd8664da12a5fcec267a153bd
yeshyungseok <yeshyungseok@naver.com>
2023-03-15 12:06:59 +0900

    cpp00 01 fixe

--- cpp/cpp00/ex02/account.cpp
+++ cpp/cpp00/ex02/account.cpp
@@ -1,41 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyungseok <hyungseok@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/13 13:33:24 by hyungnoh          #+#    #+#             */
-/*   Updated: 2023/03/14 19:16:24 by hyungseok        ###   ########.fr       */
+/*   Created: 2023/03/14 21:16:19 by hyungseok         #+#    #+#             */
+/*   Updated: 2023/03/14 21:16:35 by hyungseok        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "phonebook.hpp"
+#include "account.hpp"
 
 int	main(void)
 {
-	PhoneBook	phonebook;
-	std::string	cmd;
-	int			idx = 0, cnt = 0;
-
-	for (;;)
-	{
-		std::getline(std::cin, cmd);
-		if (cmd == "ADD")
-		{
-			Contact contact;
-			contact.create();
-			idx %= 8;
-			phonebook.add(idx, contact);
-			idx++;
-			cnt++;
-		}
-		else if (cmd == "SEARCH")
-		{
-			phonebook.show(cnt);
-		}
-		else if (cmd == "EXIT")
-			exit(0);
-	}
 	return 0;
 }
