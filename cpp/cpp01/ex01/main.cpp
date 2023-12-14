b073ce213309b4447acd501b0d70fe7379447591
Hyungseok Noh <hyungnoh@c3r6s6.42seoul.kr>
2023-12-14 17:30:36 +0900

    start cpp04

--- cpp/cpp01/ex01/main.cpp
+++ cpp/cpp01/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyungseok <hyungseok@student.42.fr>        +#+  +:+       +#+        */
+/*   By: hyungnoh <hyungnoh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 12:49:36 by hyungseok         #+#    #+#             */
-/*   Updated: 2023/12/12 19:15:49 by hyungseok        ###   ########.fr       */
+/*   Updated: 2023/12/14 17:24:47 by hyungnoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main()
 {
-    Zombie *zombies = zombieHorde(10, "cadet");
+    Zombie *zombies = zombieHorde(10, "zombie");
 
     for (int i = 0; i < 10; i++)
         zombies[i].announce();
--- cpp/cpp01/ex01/zombieHorde.cpp
+++ cpp/cpp01/ex01/zombieHorde.cpp
@@ -7,7 +7,7 @@ Zombie* zombieHorde( int N, std::string name ) {
 
   for (int i = 0; i < N; i++) {
     stringStream.str(std::string());
-        stringStream << i + 1;
+    stringStream << i + 1;
     zombies[i].setName(name + "_" + stringStream.str());
   }
   return zombies;
